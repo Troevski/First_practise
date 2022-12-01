921230d7091631b56d052a68a8916af9972063d1
Troevski <troevski1@mail.ru>
2022-12-01 17:16:50 +0100

    firts step done

---
+++ script.js
@@ -0,0 +1,16 @@
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели ?' ,'' );
+const personalMovieDB = {
+    count:{numberOfFilms},
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+};
+const a = prompt('Один из пофвопф ?',''),
+    b = prompt('Один asdasdasd ?',''),
+    c = prompt('Один asdasdasd ?',''),
+    d = prompt('Один asdasdasd ?','');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+    console.log(personalMovieDB);
